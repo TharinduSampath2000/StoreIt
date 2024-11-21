f7109da172ac267c0edf9535f9dc4433aaa30b2c
TharinduSampath2000 <stharindu444@gmail.com>
2024-11-21 13:38:35 +0530

    Fixed deployment issues

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,7 +5,6 @@ import { appwriteConfig } from "@/lib/appwrite/config";
 import { Query, ID } from "node-appwrite";
 import { parseStringify } from "@/lib/utils";
 import { cookies } from "next/headers";
-import { avatarPlaceholderUrl } from "@/constants";
 import { redirect } from "next/navigation";
 
 const getUserByEmail = async (email: string) => {
@@ -59,7 +58,7 @@ export const createAccount = async ({
       {
         fullName,
         email,
-        avatar: avatarPlaceholderUrl,
+        avatar: "/assets/images/avatar.png",
         accountId,
       },
     );
